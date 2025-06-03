9ef897da7b056e76c2c9b06d19ec6e8a30145419
jmagarinos <jmagarinos@itba.edu.ar>
2025-06-03 18:15:13 -0300

    Lambda layer and credentials adjustments

--- lambda.tf
+++ lambda.tf
@@ -30,56 +30,59 @@ resource "aws_lambda_function" "vianda_writer" {
 resource "aws_lambda_function" "vianda_buy" {
   function_name = "vianda-buy"
 
-  role          = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/LabRole"
+  role = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/LabRole"
 
-  runtime       = "python3.12"
-  handler       = "lambda_function.lambda_handler"
+  runtime = "python3.12"
+  handler = "lambda_function.lambda_handler"
 
-  filename      = "${path.module}/scripts/lambda_vianda_buy.zip"
+  filename = "${path.module}/scripts/lambda_vianda_buy.zip"
 
-  
+
 
   environment {
     variables = {
       DB_HOST     = aws_db_instance.postgres.address
       DB_NAME     = "lunchbox"
-      DB_USER     = "lunchbox_user"
+      DB_USER     = var.db_username
       DB_PASSWORD = var.db_password
     }
   }
 
   timeout = 10
 
-    vpc_config {
+  vpc_config {
     subnet_ids         = [for subnet in aws_subnet.private : subnet.id]
     security_group_ids = [module.sg.security_group_id]
   }
+
+  layers = [aws_lambda_layer_version.psycopg2_layer.arn]
+
 }
 
 resource "aws_lambda_function" "vianda_delete" {
   function_name = "vianda-delete"
 
-  role          = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/LabRole"
+  role = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/LabRole"
 
-  runtime       = "python3.12"
-  handler       = "lambda_function.lambda_handler"
+  runtime = "python3.12"
+  handler = "lambda_function.lambda_handler"
 
-  filename      = "${path.module}/scripts/lambda_vianda_delete.zip"
+  filename = "${path.module}/scripts/lambda_vianda_delete.zip"
 
-  
+
 
   environment {
     variables = {
       DB_HOST     = aws_db_instance.postgres.address
       DB_NAME     = "lunchbox"
-      DB_USER     = "lunchbox_user"
+      DB_USER     = var.db_username
       DB_PASSWORD = var.db_password
     }
   }
 
   timeout = 10
 
-    vpc_config {
+  vpc_config {
     subnet_ids         = [for subnet in aws_subnet.private : subnet.id]
     security_group_ids = [module.sg.security_group_id]
   }
@@ -88,18 +91,18 @@ resource "aws_lambda_function" "vianda_delete" {
 resource "aws_lambda_function" "vianda_list" {
   function_name = "vianda-list"
 
-  role          = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/LabRole"
+  role = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/LabRole"
 
-  runtime       = "python3.12"
-  handler       = "lambda_function.lambda_handler"
+  runtime = "python3.12"
+  handler = "lambda_function.lambda_handler"
 
-  filename      = "${path.module}/scripts/lambda_vianda_list.zip"
+  filename = "${path.module}/scripts/lambda_vianda_list.zip"
 
   environment {
     variables = {
       DB_HOST     = aws_db_instance.postgres.address
       DB_NAME     = "lunchbox"
-      DB_USER     = "lunchbox_user"
+      DB_USER     = var.db_username
       DB_PASSWORD = var.db_password
     }
   }
@@ -110,17 +113,20 @@ resource "aws_lambda_function" "vianda_list" {
     subnet_ids         = [for subnet in aws_subnet.private : subnet.id]
     security_group_ids = [module.sg.security_group_id]
   }
+
+  layers = [aws_lambda_layer_version.psycopg2_layer.arn]
+
 }
 
 resource "aws_lambda_function" "vianda_get" {
   function_name = "vianda-get"
 
-  role          = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/LabRole"
+  role = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/LabRole"
 
-  runtime       = "python3.12"
-  handler       = "lambda_function.lambda_handler"
+  runtime = "python3.12"
+  handler = "lambda_function.lambda_handler"
 
-  filename      = "${path.module}/scripts/lambda_vianda_get.zip"
+  filename = "${path.module}/scripts/lambda_vianda_get.zip"
 
   environment {
     variables = {
@@ -136,6 +142,21 @@ resource "aws_lambda_function" "vianda_get" {
   vpc_config {
     subnet_ids         = [for subnet in aws_subnet.private : subnet.id]
     security_group_ids = [module.sg.security_group_id]
+  }
+
+  layers = [aws_lambda_layer_version.psycopg2_layer.arn]
+
+}
+
+resource "aws_lambda_layer_version" "psycopg2_layer" {
+  layer_name          = "psycopg2-lunchbox"
+  compatible_runtimes = ["python3.12"]
+  description         = "psycopg2-binary for Python 3.12"
+
+  filename = "${path.module}/layers/psycopg2-layer.zip"
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
